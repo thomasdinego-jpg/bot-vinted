45c2006c027d47e124f405c96e4e9a5076ec9a05
thomasdinego-jpg <thomas.dinego@gmail.com>
2025-07-19 04:08:52 +0200

    Update worker.py

--- worker.py
+++
@@ -1,8 +0,0 @@
-import time
-from main import scrape_vinted, send_telegram_message
-
-send_telegram_message("✅ Worker Render démarré (scraping Vinted en boucle) 🟢")
-
-while True:
-    scrape_vinted()
-    time.sleep(480)
